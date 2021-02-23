58ad6f6520fd29b2fa61921fa4a79947c010fdf7
Kenneth Pham <kennethpham@elbertsoftware.com>
2021-02-23 00:55:27 -0800

    Finished quizes and exercises from Lesson 14

---
+++ Term 1/14.9. Quiz - Estimate Volatility/volatility_estimation.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+
+def estimate_volatility(prices, l):
+    """Create an exponential moving average model of the volatility of a stock
+    price, and return the most recent (last) volatility estimate.
+
+    Parameters
+    ----------
+    prices : pandas.Series
+        A series of adjusted closing prices for a stock.
+
+    l : float
+        The 'lambda' parameter of the exponential moving average model. Making
+        this value smaller will cause the model to weight older terms less 
+        relative to more recent terms.
+
+    Returns
+    -------
+    last_vol : float
+        The last element of your exponential moving averge volatility model series.
+    """
+
+    # calculate log returns
+    log_returns = np.log(prices) - np.log(prices.shift(1))
+
+    # see ewm.png for alternative formula to calculate EWMA from log returns by using alpha
+    alpha = 1 - l
+    volatility = np.sqrt((log_returns**2).ewm(alpha=alpha, adjust=True).mean())
+
+    # The exponential moving average model of volatility tracks changes in the volatility. The lambda parameter determines how responsive the estimate of the daily volatility is to the most recent daily return.
+    return volatility.iloc[-1]
+
+
+def test_run(filename='data.csv'):
+    """Test run get_most_volatile() with stock prices from a file."""
+    prices = pd.read_csv(filename, parse_dates=[
+                         'date'], index_col='date', squeeze=True)
+    print("Most recent volatility estimate: {:.6f}".format(
+        estimate_volatility(prices, 0.7)))
+
+
+if __name__ == '__main__':
+    test_run()
