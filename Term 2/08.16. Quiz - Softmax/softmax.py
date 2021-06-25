c74c2d4e8537343f663f299369f3b1c30f3af349
Kenneth Pham <kennethpham@elbertsoftware.com>
2021-06-25 06:31:23 -0700

    Term 2 lessons up to project 6

---
+++ Term 2/08.16. Quiz - Softmax/softmax.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Write a function that takes as input a list of numbers, and returns
+# the list of values given by the softmax function.
+
+
+def softmax(score_list):
+    exp_list = np.exp(score_list)
+    return np.divide(exp_list, exp_list.sum())
---
+++ Term 2/08.21. Quiz - Cross Entropy/solution.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def cross_entropy(Y, P):
+    Y = np.float_(Y)
+    P = np.float_(P)
+    return -np.sum(Y * np.log(P) + (1 - Y) * np.log(1 - P))
