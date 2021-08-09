23c3e0b253c898a93083806b805ad102430fc98d
Kenneth Pham <kennethpham@elbertsoftware.com>
2021-08-09 03:14:53 -0700

    Completed extra curricular 5. Machine Learning

---
+++ 5. Machine Learning/01.15. Linear Regression in Scikit-Learn/gapminder1.py
@@ -0,0 +1,14 @@
+# TODO: Add import statements
+
+
+# Assign the dataframe to this variable.
+# TODO: Load the data
+bmi_life_data = None 
+
+# Make and fit the linear regression model
+#TODO: Fit the model and Assign it to bmi_life_model
+bmi_life_model = None
+
+# Make a prediction using the model
+# TODO: Predict life expectancy for a BMI value of 21.07931
+laos_life_exp = None
---
+++ 5. Machine Learning/04.17. Decision Trees in sklearn/quiz.py
@@ -0,0 +1,25 @@
+# Import statements 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+
+# Read the data.
+data = np.asarray(pd.read_csv('data.csv', header=None))
+# Assign the features to the variable X, and the labels to the variable y. 
+X = data[:,0:2]
+y = data[:,2]
+
+# TODO: Create the decision tree model and assign it to the variable model.
+# You won't need to, but if you'd like, play with hyperparameters such
+# as max_depth and min_samples_leaf and see what they do to the decision
+# boundary.
+model = None
+
+# TODO: Fit the model.
+
+# TODO: Make predictions. Store them in the variable y_pred.
+y_pred = None
+
+# TODO: Calculate the accuracy and assign it to the variable acc.
+acc = None
