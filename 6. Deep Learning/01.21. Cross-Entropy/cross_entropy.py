dd671a09a497d303c438e00091d45cd659895383
Kenneth Pham <kennethpham@elbertsoftware.com>
2021-08-14 06:11:48 -0700

    Completed Deep Learning Extra Curricular

---
+++ 6. Deep Learning/01.21. Cross-Entropy/cross_entropy.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# Write a function that takes as input two lists Y, P,
+# and returns the float corresponding to their cross-entropy.
+def cross_entropy(Y, P):
+    return -np.sum([y * np.log(p) + (1 - y) * np.log(1 -p) for y, p in zip(Y, P)])
