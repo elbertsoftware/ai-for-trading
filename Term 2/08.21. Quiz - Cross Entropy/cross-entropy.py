5ef4dddfdd202125902b9e4757719542b986ae2d
Kenneth Pham <kennethpham@elbertsoftware.com>
2021-11-08 03:33:45 -0800

    Completed Term 2 Lesson 8 Introduction to Neural Networks

--- Term 2/08.21. Quiz - Cross Entropy/cross-entropy.py
+++ Term 2/08.21. Quiz - Cross Entropy/cross-entropy.py
@@ -1,9 +1,8 @@
 import numpy as np
 
+
 # As in the video, Y in the quiz is for the category, and P is the probability.
 # Write a function that takes as input two lists Y, P,
 # and returns the float corresponding to their cross-entropy.
-
-
 def cross_entropy(Y, P):
     return -np.sum([y * np.log(p) + (1 - y) * np.log(1 - p) for y, p in zip(Y, P)])
